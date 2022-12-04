e8bdc61f901b19b4acb2d201000aedd54e31fcc7
sultan <stazabekuulu@gmail.com>
2022-12-05 01:03:24 +0600

    Updates for models wallet

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account
+
 
 # Register your models here.
+@admin.register(Account)
+class ProfilesAdmin(admin.ModelAdmin):
+    list_display = ('owners', 'summ_in_dollar', 'title_valute')
--- wallet/models.py
+++ wallet/models.py
@@ -5,10 +5,12 @@ from valuta.models import Value
 
 class Account(models.Model):
     owners = models.ForeignKey(Users, on_delete=models.PROTECT)
-    summ = models.IntegerField()
-    count_valutes = models.ForeignKey(Value, on_delete=models.PROTECT)
+    summ_in_dollar = models.DecimalField(decimal_places=3, max_digits=40)
+    title_valute = models.ForeignKey(Value, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.owners
 
-class Desire(models.Model):
-    first_sum = models.IntegerField()
-    want_sum = models.IntegerField()
+# class Desire(models.Model):
+#     first_sum = models.IntegerField()
+#     want_sum = models.IntegerField()
