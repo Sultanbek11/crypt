544d8c2baab2cd6c4f77c38dd75ce9445c726525
Sultanbek11 <110616560+Sultanbek11@users.noreply.github.com>
2022-12-29 13:57:55 +0600

    Update urls.py

--- krypta/urls.py
+++ krypta/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('users/', include('users.urls')),
-    path('valuta/', include('valuta.urls')),
+    path('', include('valuta.urls')),
     path('wallet/', include('wallet.urls')),
 ]
